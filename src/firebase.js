8fd5497b18a576f920a812f3147b0d67450a0088
Alexmust2 <alex.must2003@mail.ru>
2024-04-14 22:23:46 +0300

    hot-fixes

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,28 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
 
-const firebaseConfig = { 
-    apiKey: process.env.VUE_APP_FIREBASE_API_KEY, 
-    authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN, 
-    projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID, 
-    storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET, 
-    messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID, 
-    appId: process.env.VUE_APP_FIREBASE_APP_ID 
+const firebaseConfig = {
+    apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+    authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.VUE_APP_FIREBASE_APP_ID
 };
+
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+const db = getFirestore(app);
+
+async function fetchAllUsers() {
+    try {
+        const usersCol = collection(db, 'users');
+        const snapshot = await getDocs(usersCol);
+        const userList = snapshot.docs.map(doc => doc.data());
+        console.log(userList);
+        return userList;  // Возвращает массив объектов данных каждого документа
+    } catch (error) {
+        console.error("Ошибка при получении документов:", error);
+    }
+}
+
+export { db, fetchAllUsers };
--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,17 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Tasks from '../views/TasksView.vue'
 import HomeView from '@/views/HomeView.vue'
 import NotFoundView from '@/views/NotFoundView.vue'
+import { decode } from 'js-base64';
+import { db } from '../firebase';
+import { doc, getDoc } from "firebase/firestore";
+
 
 const routes = [
   {
     path: '/tasks',
     name: 'tasks',
-    component: Tasks
+    component: Tasks,
+    meta: { requiresAuth: true }
   },
   {
     path: '/',
