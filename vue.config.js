42c52a97db50b78081d7407e1fabba163e402833
Alexmust2 <alex.must2003@mail.ru>
2024-05-22 15:12:13 +0300

    add pwa

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,26 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  pwa: {
+    name: 'tarkov-helper',
+    themeColor: '#2c3e50',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    manifestOptions: {
+      display: 'standalone',
+      background_color: '#2c3e50',
+      icons: [
+        {
+          src: 'icon.png',
+          sizes: '213x213',
+          type: 'image/png'
+        }
+      ]
+    },
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      swSrc: 'dev/sw.js',
+    }
+  }
 })
